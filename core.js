9090b5cfe7abb220e29ca1f9021bf3fd509aa123
Joe Gaudet <joe@matygo.com>
2012-11-02 17:58:55 -0700

    first commit

---
+++ core.js
@@ -0,0 +1,21 @@
+// ==========================================================================
+// Project:   SproutcoreWysiwyg
+// Copyright: @2012 My Company, Inc.
+// ==========================================================================
+/*globals SproutcoreWysiwyg */
+
+/** @namespace
+
+  My cool new framework.  Describe your framework.
+  
+  @extends SC.Object
+*/
+SproutcoreWysiwyg = SC.Object.create(
+  /** @scope SproutcoreWysiwyg.prototype */ {
+
+  NAMESPACE: 'SproutcoreWysiwyg',
+  VERSION: '0.1.0',
+
+  // TODO: Add global constants or singleton objects needed by your app here.
+
+}) ;
